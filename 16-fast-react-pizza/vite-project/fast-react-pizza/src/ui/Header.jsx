8abe69d44b0ca447c736486ae725522addbedc8a
Xu <whitenight.xiaxu@gmail.com>
2023-10-09 16:52:29 +0800

    tailwind config

--- 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,16 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import SearchOrder from "../features/order/SearchOrder"
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React App</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React App
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
-  )
+  );
 }
