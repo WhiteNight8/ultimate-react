73b22a8cc45961bc84395532acd2b25718e0ac57
Xu <whitenight.xiaxu@gmail.com>
2023-10-11 16:15:11 +0800

    responsive design

--- 16-fast-react-pizza/vite-project/fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/vite-project/fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">joeyXX</div>;
+  return <div className="hidden text-sm font-semibold">joeyXX</div>;
 }
 
 export default Username;
