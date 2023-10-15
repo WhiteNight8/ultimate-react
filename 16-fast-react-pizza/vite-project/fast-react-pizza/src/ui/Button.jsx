f7511a7de0a228ced63f901441996a6cc14b1ae0
Xu <whitenight.xiaxu@gmail.com>
2023-10-15 17:55:30 +0800

    style button

---
+++ 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button({ children, disabled }) {
+  return (
+    <button
+      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase
+  tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300
+  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4"
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
