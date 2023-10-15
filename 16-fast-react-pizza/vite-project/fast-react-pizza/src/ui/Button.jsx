d1d59f78e484b0eaa67f171b5cba9f9a25e476bf
Xu <whitenight.xiaxu@gmail.com>
2023-10-15 18:22:32 +0800

    style link button

--- 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Button.jsx
@@ -1,12 +1,18 @@
-export default function Button({ children, disabled }) {
-  return (
-    <button
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase
-  tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300
-  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4"
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
+import { Link } from 'react-router-dom'
+
+export default function Button({ children, disabled, to }) {
+    const className =
+        'inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4'
+    if (to) {
+        return (
+            <Link to={to} className={className}>
+                {children}
+            </Link>
+        )
+    }
+    return (
+        <button className={className} disabled={disabled}>
+            {children}
+        </button>
+    )
 }
