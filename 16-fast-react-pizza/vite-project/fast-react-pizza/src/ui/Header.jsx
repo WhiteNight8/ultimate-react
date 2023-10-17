d811f7a7b31f328ba59e39beb2eb2f5eaae38ab2
Xu <whitenight.xiaxu@gmail.com>
2023-10-17 16:33:21 +0800

    reset font family

--- 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/vite-project/fast-react-pizza/src/ui/Header.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/Username";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
+import Username from '../features/user/Username'
 
 export default function Header() {
-  return (
-    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
-        Fast React App
-      </Link>
-      <SearchOrder />
-      <Username />
-    </header>
-  );
+    return (
+        <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6 font-pizza">
+            <Link to="/" className="tracking-widest">
+                Fast React App
+            </Link>
+            <SearchOrder />
+            <Username />
+        </header>
+    )
 }
