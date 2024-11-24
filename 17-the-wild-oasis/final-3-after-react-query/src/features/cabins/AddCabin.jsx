9b2753258a06840e0edb2058b584687622060d00
Xu <whitenight.xiaxu@gmail.com>
2024-11-24 16:28:33 +0800

    modal compound

--- 17-the-wild-oasis/final-3-after-react-query/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/final-3-after-react-query/src/features/cabins/AddCabin.jsx
@@ -2,18 +2,41 @@ import Button from "../../ui/Button.jsx";
 import {useState} from "react";
 import Modal from "../../ui/Modal.jsx";
 import CreateCabinForm from "./CreateCabinForm.jsx";
+import CabinTable from "./CabinTable.jsx";
 
-function AddCabin(){
-    const [isOpenModal, setOpenModal] = useState(false);
+// function AddCabin(){
+//     const [isOpenModal, setOpenModal] = useState(false);
+//
+//     return (
+//         <div>
+//             <Button onClick={() => setOpenModal((show) => !show)}>
+//                 Add new cabin
+//             </Button>
+//             {isOpenModal && (<Modal onClose={()=>setOpenModal(false)}>
+//                 <CreateCabinForm onCloseModal={()=>setOpenModal(false)}/>
+//             </Modal>)}
+//         </div>
+//     )
+// }
 
+function  AddCabin() {
     return (
         <div>
-            <Button onClick={() => setOpenModal((show) => !show)}>
-                Add new cabin
-            </Button>
-            {isOpenModal && (<Modal onClose={()=>setOpenModal(false)}>
-                <CreateCabinForm onCloseModal={()=>setOpenModal(false)}/>
-            </Modal>)}
+            <Modal>
+                <Modal.Open opens='cabin-form'>
+                    <Button>Add new cabin</Button>
+                </Modal.Open>
+                <Modal.Window name='cabin-form'>
+                    <CreateCabinForm/>
+                </Modal.Window>
+
+                {/*<Modal.Open opens='table'>*/}
+                {/*    <Button>Show table</Button>*/}
+                {/*</Modal.Open>*/}
+                {/*<Modal.Window name='table'>*/}
+                {/*    <CabinTable/>*/}
+                {/*</Modal.Window>*/}
+            </Modal>
         </div>
     )
 }
