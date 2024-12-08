19e4e9b045dcc275102d315e907a6916235be27d
Xu <whitenight.xiaxu@gmail.com>
2024-12-08 15:19:23 +0800

    bug fix

--- 17-the-wild-oasis/final-3-after-react-query/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/final-3-after-react-query/src/features/cabins/CabinTable.jsx
@@ -34,11 +34,8 @@ function CabinTable() {
     let filteredCabins
     if(filterValue === "all")  filteredCabins = cabins;
     if(filterValue === "no-discount")  filteredCabins = cabins.filter( cabin => cabin.discount===0);
-    if(filterValue === "no-discount")  filteredCabins = cabins.filter( cabin => cabin.discount > 0);
-
-
-
-
+    if(filterValue === "with-discount")  filteredCabins = cabins.filter( cabin => cabin.discount > 0);
+    
     return (
         <Menus>
             <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
