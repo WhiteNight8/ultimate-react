28ae990ab5cd2f26ba21d145e6fdc4c066427861
Xu <whitenight.xiaxu@gmail.com>
2025-01-12 16:43:22 +0800

    pagination

--- 17-the-wild-oasis/final-3-after-react-query/src/ui/Pagination.jsx
+++ 17-the-wild-oasis/final-3-after-react-query/src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {useSearchParams} from "react-router-dom";
 import {HiChevronLeft, HiChevronRight} from "react-icons/hi2";
+import {PAGE_SIZE} from "../utils/constants.js";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -57,7 +58,7 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-const PAGE_SIZE = 2;
+
 
 export  default  function Pagination({count}) {
     const [searchParams,setSearchParams] = useSearchParams()
